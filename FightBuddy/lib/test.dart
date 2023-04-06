cecec9e3bf7294bb6cb26e12404d057becd4264f
UglyWallach <118812603+UglyWallach@users.noreply.github.com>
2023-04-06 14:35:48 +0200

    Sqltest

--- FightBuddy/lib/test.dart
+++ FightBuddy/lib/test.dart
@@ -1,5 +1,10 @@
 import 'package:mysql1/mysql1.dart';
 
+void main(){
+  Test t = Test();
+  t.main();
+}
+
 class Test {
   Future main() async {
     // Open a connection (testdb should already exist)
@@ -9,14 +14,14 @@ class Test {
     user: 'fightbud@a337869',
     password: 'fightbuddy',
     db: 'aifboxning_se_db_4'
+    
       ));
-    var results = await connection.query('select * from tableName');
+    var results = await connection.query('select * from users');
     for (var row in results) {
-      print('${row[0]}');
+      print('${row[1]}');
     }
 
     // Finally, close the connection
     await connection.close();
   }
 }
-
