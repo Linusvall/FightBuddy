ab840458c552e2da5995b51a5a42061787262fa5
sonnysoso <129958348+sonnysoso@users.noreply.github.com>
2023-04-06 14:10:14 +0200

    test2
    test2

--- FightBuddy/lib/test.dart
+++ FightBuddy/lib/test.dart
@@ -1,16 +1,17 @@
 import 'package:mysql1/mysql1.dart';
 
 class Test {
-  Future main() async {
+
+  static Future<void> main() async {
     // Open a connection (testdb should already exist)
     final connection = await MySqlConnection.connect(ConnectionSettings(
-    host: 'mysql681.loopia.se', 
-    port: 3306,
-    user: 'fightbud@a337869',
-    password: 'fightbuddy',
-    db: 'aifboxning_se_db_4'
-      ));
-    var results = await connection.query('select * from tableName');
+      host: 'mysql681.loopia.se', 
+      port: 3306,
+      user: 'fightbud@a337869',
+      password: 'fightbuddy',
+      db: 'aifboxning_se_db_4',
+    ));
+    var results = await connection.query('select * from yourTableName');
     for (var row in results) {
       print('${row[0]}');
     }
---
+++ FightBuddy/lib/test2.dart
@@ -0,0 +1,5 @@
+void main(){
+  var firstName = 'Sonny';
+  String lastName = 'Solsona';
+  print(firstName + ' ' + lastName);
+  }
