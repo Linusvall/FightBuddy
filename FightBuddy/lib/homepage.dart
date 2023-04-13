804d1c73914dae7f06e0f736262585dcba483403
sonnysoso <129958348+sonnysoso@users.noreply.github.com>
2023-04-13 10:11:47 +0200

    BottomNavigationBar

--- FightBuddy/lib/homepage.dart
+++ FightBuddy/lib/homepage.dart
@@ -1,17 +1,36 @@
 import 'package:flutter/material.dart';
+import 'profile.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
-  _HomePageState createState() => _HomePageState();
-}
+  State<HomePage> createState() => _HomePageState();
+} 
 
 class _HomePageState extends State<HomePage> {
+  int currentPage = 0;
+  List<Widget> pages = const[
+    HomePage(), 
+    ProfilePage()
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: const Text('Home'), centerTitle: true),
-        body: Center());
+        body: pages[currentPage] ,
+        bottomNavigationBar: NavigationBar(
+        destinations: const [ 
+          NavigationDestination(icon: Icon(Icons.home), label: 'Hem'),
+          NavigationDestination(icon: Icon(Icons.person), label: 'Profil'),
+        ],
+        onDestinationSelected: (int index){
+          setState(() {
+            currentPage = index;
+            });
+          },
+          selectedIndex: currentPage,
+        ),
+    );
   }
 }
--- FightBuddy/lib/main.dart
+++ FightBuddy/lib/main.dart
@@ -3,14 +3,17 @@ import 'register.dart';
 import 'login.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: WelcomePage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
