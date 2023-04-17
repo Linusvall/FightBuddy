fe53a82f831dcf4258c5de73b7b29a2ca9e5c0bd
Linusvall <105202392+Linusvall@users.noreply.github.com>
2023-04-17 19:39:47 +0200

    email & globala variabler

--- FightBuddy/lib/gender.dart
+++ FightBuddy/lib/gender.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'globals.dart' as global;
 
-
-void main(){
+void main() {
   runApp(MyApp());
 }
 
@@ -14,33 +14,36 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class GenderPage extends StatefulWidget{
- @override
+class GenderPage extends StatefulWidget {
+  @override
   _genderPageState createState() => _genderPageState();
+}
+
+class _genderPageState extends State<GenderPage> {
+  pressed() {
+    print(global.hello);
+    global.hello = "ändrad";
+    print(global.hello);
   }
 
-  class _genderPageState extends State<GenderPage>{
-    bool _womanHasBeenPressed = false;
-    bool _nothingHasBeenPressed = false;
-    bool _manHasBeenPressed = false;
-    var gender = '';
+  bool _womanHasBeenPressed = false;
+  bool _nothingHasBeenPressed = false;
+  bool _manHasBeenPressed = false;
+  var gender = '';
 
-    pressedbutton(String button){
+  pressedbutton(String button) {
     _nothingHasBeenPressed = false;
     _manHasBeenPressed = false;
     _womanHasBeenPressed = false;
 
-
-    if(button == 'man'){
+    if (button == 'man') {
       _manHasBeenPressed = true;
       gender = 'man';
-    }
-    else if(button == 'woman'){
+    } else if (button == 'woman') {
       _womanHasBeenPressed = true;
       gender = 'woman';
-    }
-    else{
-      _nothingHasBeenPressed = true; 
+    } else {
+      _nothingHasBeenPressed = true;
       gender = 'nothing';
     }
   }
@@ -48,119 +51,114 @@ class GenderPage extends StatefulWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Kön"), centerTitle: true,),
-      body: Center(
-        
-        child: Column(
-          children: <Widget>[
-            Padding( padding: const EdgeInsets.all(100),
-              child: const Text("Jag är en ",
-                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),  
-              )
-              ),
-              Padding(padding: const EdgeInsets.all(5),
+        appBar: AppBar(
+          title: Text("Kön"),
+          centerTitle: true,
+        ),
+        body: Center(
+            child: Column(children: <Widget>[
+          Padding(
+              padding: const EdgeInsets.all(100),
+              child: const Text(
+                "Jag är en ",
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+              )),
+          Padding(
+              padding: const EdgeInsets.all(5),
               child: ElevatedButton(
-                
-                style: ElevatedButton.styleFrom(fixedSize: const Size(300,60),
-                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15.0)),
-                  primary: _womanHasBeenPressed ? Color.fromRGBO(0,181,169,100) : Colors.white,
-                  
-                ),
-                onPressed: () => setState((){
-                  pressedbutton("woman");
-                }
-                ),
-                child: Row(
-                  children: [
-                    Align(alignment: Alignment.centerLeft,
-                      child: Text("Kvinna                                                      ",
-                      style: TextStyle(color: Color.fromRGBO(112,112,112,100))
-                      )
-                      ),
-                    Align(alignment: Alignment.centerRight,
-                      child: Icon(Icons.check)
-                    )
-                  ],
-                )
-                )
-                ),
-                Padding(padding: const EdgeInsets.all(5),
-              child: ElevatedButton(
-                
-                style: ElevatedButton.styleFrom(fixedSize: const Size(300,60),
-                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15.0)),
-                  primary: _manHasBeenPressed ? Color.fromRGBO(0,181,169,100) : Colors.white,
-                ),
-                onPressed: () => setState(() {
-                  pressedbutton("man");
-                }
-                ),
-                child: Row(
-                  children: [
-                    Align(alignment: Alignment.centerLeft,
-                      child: Text("Man                                                          ",
-                      style: TextStyle(color: Color.fromRGBO(112,112,112,100))
-                      )
-                    ),
-                    Align(alignment: Alignment.centerRight,
-                      child: Icon(Icons.check)
-                    )
-                  ],
-                )
-                )
-                ),
-
-                Padding(padding: const EdgeInsets.all(5),
-              child: ElevatedButton(
-                
-                style: ElevatedButton.styleFrom(fixedSize: const Size(300,60),
-                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15.0)),
-                  primary : _nothingHasBeenPressed ? const Color.fromRGBO(0,181,169,100) : Colors.white,
-                ),
-                onPressed: () => setState(() {
-                  pressedbutton("nothing");
-                }
-                ),
-                child: Row(
-                  children: [
-                    Align(alignment: Alignment.centerLeft,
-                      child: Text("Vill inte ange                                          ",
-                      style: TextStyle(color: Color.fromRGBO(112,112,112,100))
-                      )
-                    ),
-                    Align(alignment: Alignment.centerRight,
-                      child: Icon(Icons.check)
-                    )
-                  ],
-                )
-                )
-                ), 
-                Padding(padding: const EdgeInsets.all(70),
-                child: ElevatedButton(
-                  onPressed: () => print("pressed"),
                   style: ElevatedButton.styleFrom(
-                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15.0)),
-                    primary: Color.fromRGBO(0,181,169,100),
-                    minimumSize: Size(250,70)
+                    fixedSize: const Size(300, 60),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15.0)),
+                    primary: _womanHasBeenPressed
+                        ? Color.fromRGBO(0, 181, 169, 100)
+                        : Colors.white,
                   ),
-                  
-                  child: Text("Gå vidare",
-                  style: TextStyle(color: Colors.white,
-                  fontSize: 20,
-
-                  )
+                  onPressed: () => setState(() {
+                        pressedbutton("woman");
+                      }),
+                  child: Row(
+                    children: [
+                      Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                              "Kvinna                                                      ",
+                              style: TextStyle(
+                                  color: Color.fromRGBO(112, 112, 112, 100)))),
+                      Align(
+                          alignment: Alignment.centerRight,
+                          child: Icon(Icons.check))
+                    ],
+                  ))),
+          Padding(
+              padding: const EdgeInsets.all(5),
+              child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    fixedSize: const Size(300, 60),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15.0)),
+                    primary: _manHasBeenPressed
+                        ? Color.fromRGBO(0, 181, 169, 100)
+                        : Colors.white,
                   ),
-
-                )
-                )
-          ]
-        )
-      )
-    );
+                  onPressed: () => setState(() {
+                        pressedbutton("man");
+                      }),
+                  child: Row(
+                    children: [
+                      Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                              "Man                                                          ",
+                              style: TextStyle(
+                                  color: Color.fromRGBO(112, 112, 112, 100)))),
+                      Align(
+                          alignment: Alignment.centerRight,
+                          child: Icon(Icons.check))
+                    ],
+                  ))),
+          Padding(
+              padding: const EdgeInsets.all(5),
+              child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    fixedSize: const Size(300, 60),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15.0)),
+                    primary: _nothingHasBeenPressed
+                        ? const Color.fromRGBO(0, 181, 169, 100)
+                        : Colors.white,
+                  ),
+                  onPressed: () => setState(() {
+                        pressedbutton("nothing");
+                      }),
+                  child: Row(
+                    children: [
+                      Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                              "Vill inte ange                                          ",
+                              style: TextStyle(
+                                  color: Color.fromRGBO(112, 112, 112, 100)))),
+                      Align(
+                          alignment: Alignment.centerRight,
+                          child: Icon(Icons.check))
+                    ],
+                  ))),
+          Padding(
+              padding: const EdgeInsets.all(70),
+              child: ElevatedButton(
+                onPressed: () => pressed(),
+                style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15.0)),
+                    primary: Color.fromRGBO(0, 181, 169, 100),
+                    minimumSize: Size(250, 70)),
+                child: Text("Gå vidare",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                    )),
+              ))
+        ])));
   }
 }
-
-  
-
-
-  
--- FightBuddy/lib/sendEmail.dart
+++ FightBuddy/lib/sendEmail.dart
@@ -1,32 +1,34 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
-void main(){
-  runApp(MyApp());
-}
+class EmailPage {
+  Future sendEmail({
+    required String name,
+    required String email,
+    required String subject,
+    required String message,
+  }) async {
+    final serviceId = 'service_izh8b2h';
+    final templateId = 'template_74y75k3';
+    final userId = 'HHAeMuM6HJ6hkYZIk';
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: emailPage(),
-    );
+    final url = Uri.parse("https://api.emailjs.com/api/v1.0/email/send");
+    final respone = await http.post(url,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: json.encode({
+          'service_id': serviceId,
+          'template_id': templateId,
+          'user_id': userId,
+          'template_params': {
+            'user_name': name,
+            'user_email': email,
+            'user_subject': subject,
+            'user_message': message,
+          }
+        }));
   }
 }
-
-class emailPage extends StatelessWidget{
- @override
-
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Kön"), centerTitle: true,),
-      body: Center(
-        child: ElevatedButton(
-          onPressed :() => print("Hej"),
-          child: Text("Test"),
-        )
-      )
-    );
-  }
-  }
