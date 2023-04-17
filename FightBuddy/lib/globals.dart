7ff8ccba59d3cc53e802a1a08ba050333ad12307
Linusvall <105202392+Linusvall@users.noreply.github.com>
2023-04-17 19:49:06 +0200

    Update globals.dart

--- FightBuddy/lib/globals.dart
+++ FightBuddy/lib/globals.dart
@@ -1,1 +1,10 @@
-String hello = 'hello';
+import 'dart:collection';
+
+String gender = '';
+int age = 0;
+int weight = 0;
+int height = 0;
+String information = '';
+String club = '';
+List<String> martialArts = List.empty();
+HashMap martialArtExperience = new HashMap<String, String>();
