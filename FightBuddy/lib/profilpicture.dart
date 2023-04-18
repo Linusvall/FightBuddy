0beff3bccf005d9494a09dd966def1879ee91211
UglyWallach <118812603+UglyWallach@users.noreply.github.com>
2023-04-18 13:08:31 +0200

    Liten ändring för att bilden ska sparas på användarens telefon

--- FightBuddy/lib/profilpicture.dart
+++ FightBuddy/lib/profilpicture.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
-
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:image_picker/image_picker.dart';
@@ -34,13 +35,23 @@ class ProfilePictureState extends State<ProfilePicture> {
       final image = await ImagePicker().pickImage(source: source);
       if (image == null) return;
 
-      final imageTemporary = File(image.path);
+      //final imageTemporary = File(image.path);
+      final imagePermanent = await saveImagePermanently(image.path);
+
       setState(() {
-        this.image = imageTemporary;
+        this.image = imagePermanent;
       });
     } on PlatformException catch (e) {
       print('Failed to pick image: $e');
     }
+  }
+
+  Future<File> saveImagePermanently(String imagePath) async {
+    final directory = await getApplicationDocumentsDirectory();
+    final name = basename(imagePath);
+    final image = File('${directory.path}/$name');
+
+    return File(imagePath).copy(imagePath);
   }
 
   @override
